642a61a13e3d51bea4c38ad979abfe830f03c2b2
subodhgujar <subodhsgujar@gmail.com>
2022-10-07 00:23:12 -0400

    mobile and name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
     validates   :card, presence: true, format: {with: /[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}/, message: "should be in xxxx-xxxx-xxxx-xxxx format."}
 
-    validates :mobile, format: {with: /[0-9]{10}/, message: "should be 10 digits"}
+    validates :mobile, presence: true, format: {with: /[0-9]{10}/, message: "should be 10 digits"}
 
     validates :name, presence: true
     private
