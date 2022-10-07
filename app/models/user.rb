5c0ca5b5042acd18d246dd86c2ffd3327530346b
subodhgujar <subodhsgujar@gmail.com>
2022-10-07 00:19:14 -0400

    mobile and name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
     validates   :email ,format: { with: URI::MailTo::EMAIL_REGEXP, message: "is not a valid" }, presence: true, uniqueness: true
 
     validates   :card, presence: true, format: {with: /[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}/, message: "should be in xxxx-xxxx-xxxx-xxxx format."}
+
+    validates :mobile, format: {with: /[0-9]{10}/, message: "should be 10 digits"}
+
+    validates :name, presence: true
     private
 
     def downcase_email
